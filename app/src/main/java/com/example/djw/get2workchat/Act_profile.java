9117e1c2ec38f4be333c9475b7d03250edca4150
Wiwe1 <wiwe123@hotmail.com>
2018-09-30 17:50:37 +0200

    -added profile acivity      - profile picture      - profile name      - email      - phone number    - needs work on buttom text views
     - Resolved problems with XML for sent and rescived messeges. Needs to be corrected for furture use

--- app/src/main/java/com/example/djw/get2workchat/Act_profile.java
+++ app/src/main/java/com/example/djw/get2workchat/Act_profile.java
@@ -3,12 +3,11 @@ package com.example.djw.get2workchat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
-public class MainActivity extends AppCompatActivity {
+public class Act_profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
     }
 }
